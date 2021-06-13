85ecd5ab2b62af6dd15af5e1b05223e8ad58492c
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-06-13 11:11:49 +0300

    registering blueprints

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+
+bootstrap = Bootstrap()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    app.config.from_object(config_options[config_name])
+    '''cerating the app configuration'''
+    
+    # initialising the extensions
+    bootstrap.init_app(app)
+
+
+    # registering the blueprints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
+
+    
+    
+    return app
---
+++ config.py
@@ -0,0 +1,40 @@
+from logging import DEBUG
+import os
+
+from flask import config
+
+class Config:
+    '''
+    general configuration parent class
+    '''
+    pass
+
+class ProdConfig(Config):
+    '''
+    production configuration child class
+    
+        Arg:
+            Config: th parent configuration class with general configuration settings
+    '''
+
+class TestConfig(Config):
+    '''
+    Test configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+
+config_options={
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test':TestConfig
+}

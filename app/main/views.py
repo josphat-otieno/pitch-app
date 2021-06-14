7b55d944c69a4adb1ed2c314ee7ac5457f57b2ec
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-06-14 05:12:45 +0300

    added the login part and authenticate it

--- app/main/views.py
+++ app/main/views.py
@@ -31,3 +31,31 @@ def new_pitch():
         new_pitch_object = Pitch(post=post,user_id=current_user._get_current_object().id,category=category,title=title)
         new_pitch_object.save_p()
         return redirect(url_for('main.index'))
+
+
+@main.route('/comment/<int:pitch_id>', methods = ['POST','GET'])
+@login_required
+def comment(pitch_id):
+    form = CommentsForm()
+    pitch = Pitch.query.get(pitch_id)
+    all_comments = Comments.query.filter_by(pitch_id = pitch_id).all()
+    if form.validate_on_submit():
+        comment = form.comment.data 
+        pitch_id = pitch_id
+        user_id = current_user._get_current_object().id
+        new_comment = Comments(comment = comment,user_id = user_id,pitch_id = pitch_id)
+        new_comment.save_c()
+        return redirect(url_for('.comment', pitch_id = pitch_id))
+    return render_template('comment.html', form =form, pitch = pitch,all_comments=all_comments)
+
+
+@main.route('/user/<name>')
+def profile(name):
+    user = User.query.filter_by(username = name).first()
+    user_id = current_user._get_current_object().id
+    posts = Pitch.query.filter_by(user_id = user_id).all()
+    if user is None:
+        abort(404)
+
+  
+    return render_template("profile/profile.html", user = user,posts=posts)

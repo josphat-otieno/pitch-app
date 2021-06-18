1c344377f79e01cd4cc1ee7f75fb81a3e3e98b56
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-06-18 13:01:13 +0300

    redeploy

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 
 from . import db
-from datetime import datetime
+from datetime import datetime, time
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, current_user
 from . import login_manager

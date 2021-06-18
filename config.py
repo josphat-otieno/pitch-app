b2ae083e0f5237e505c8d404839a64a1c49aa35f
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-06-18 22:23:33 +0300

    change email to googlemail

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     general configuration parent class
     '''
     SECRET_KEY= os.environ.get('SECRET_KEY')
-    MAIL_SERVER= 'smtp.gmail.com'
+    MAIL_SERVER= 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
